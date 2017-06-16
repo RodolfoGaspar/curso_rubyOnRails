65d6a49f363766e89bfb85fa6cfda80cb5cba2ea
Rodolfo Gaspar <dolfogaspar@gmail.com>
2017-06-15 23:04:44 -0300

    alter case2

--- case2.rb
+++ case2.rb
@@ -1,1 +1,9 @@
-tempo_atual = ARGV.first
+tempo_atual = ARGV.first.to_i
+
+case tempo_atual
+    when 0..45 then puts("primeiro tempo")
+    when 46..90 then puts("segundo tempo")
+    when 91..100 then puts("acréscimos")
+    # when < 0 then puts ("aquecimento")    
+    else puts("tempo inválido")        
+end
